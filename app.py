984a6544b645865be54989b10c296be7e291d07d
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2024-06-18 03:37:21 +0200

    edit typo

--- app.py
+++ app.py
@@ -4,14 +4,11 @@ import matplotlib.pyplot as plt
 from stpp import StdDiffusionKernel, HawkesLam, SpatialTemporalPointProcess
 from utils import plot_combined_intensity_and_events
 
-# Set random seed for reproducibility
 np.random.seed(0)
 np.set_printoptions(suppress=True)
 
-# Sidebar for parameter settings
 st.sidebar.title("Parameter Settings")
 
-# Define parameter tooltips
 tooltips = {
     "mu": "Background intensity of the Hawkes process.",
     "beta": "Temporal Decay rate of the influence of past events.",
@@ -21,7 +18,6 @@ tooltips = {
     "grid_size": "Size of the spatial grid for plotting.",
 }
 
-# Add sliders with tooltips
 mu = st.sidebar.slider("µ", min_value=0.0, max_value=1.0, value=0.1, help=tooltips["mu"])
 beta = st.sidebar.slider("β", min_value=0.1, max_value=10.0, value=1.0, help=tooltips["beta"])
 gamma = st.sidebar.slider("γ", min_value=5.0, max_value=100.0, value=10.0, help=tooltips["gamma"])
@@ -32,8 +28,8 @@ S_x_min = -1.0
 S_x_max = 1.0
 S_y_min = -1.0
 S_y_max = 1.0
-t_slots = st.sidebar.slider("t_slots", min_value=10, max_value=2000, value=600, help=tooltips["t_slots"])
 grid_size = st.sidebar.slider("grid_size", min_value=10, max_value=200, value=30, help=tooltips["grid_size"])
+t_slots = st.sidebar.slider("t_slots", min_value=10, max_value=2000, value=600, help=tooltips["t_slots"])
 interval = 50
 
 @st.cache_data  
@@ -50,20 +46,18 @@ def generate_points(mu, beta, gamma, alpha, T_start, T_end, S_x_min, S_x_max, S_
 points, lam = generate_points(mu, beta, gamma, alpha, T_start, T_end, S_x_min, S_x_max, S_y_min, S_y_max)
 
 
-# Standard diffusion kernel formula
 diffusion_kernel_formula = r"""
 $g_t(t, t_i; \beta_i) = \exp \left( - \beta_i \|t - t_i\| \right)$
 """ +r"""
 $g_s(s, s_i; \gamma_i) = \alpha^{-1} \exp \left( - \gamma_i \|s - s_i\| \right)$
 """ 
 
-# Hawkes process formula
 hawkes_process_formula = r"$\lambda(s, t, H_t) = \mu + \sum_{(t_i, s_i) \in H_t} g_t(t, t_i) g_s(s, s_i)$"
 
 
 st.write(f"### Diffusion Kernel Formula\n\n{diffusion_kernel_formula}")
 st.write(f"### Hawkes Process Formula\n\n{hawkes_process_formula}")
-st.write(r"$g_1$, and $g_2$ are probably constants !")
+st.write(r"$g_t$, and $g_s$ are probably constants !")
 
 
 plot_combined_intensity_and_events(
